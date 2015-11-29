7aa8cdfb9343c3f8c7e6b2f0548fc4409c06a109
TheDizzler <tgillon.music@gmail.com>
2015-11-30 04:24:37 +0900

    Created base project; added SlimDX reference

---
+++ DJMixer/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DJMixer {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+			
+        }
+
+		private void buttonPlay_Click(Object sender, EventArgs e) {
+			//System.Media.SoundPlayer player = new System.Media.SoundPlayer("D:/mp3z/JPop/Tsunku/Canary Club/05. [H!PBR] Canary Club - SWEET & TOUGHNESS.mp3");
+			//music = new Microsoft.DirectX.AudioVideoPlayback.Audio(Location);
+			//SlimDX.DirectSound.
+		}
+	}
+}
